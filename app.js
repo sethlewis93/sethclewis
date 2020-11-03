55bd1ebd72b08e5272a6c2a0bcc568390c612aaf
sethlewis93 <sbmail616@protonmail.com>
2020-11-03 11:55:36 -0500

    attempts to set first dynamic route from index

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const data = require("./data.json");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+// Utilize Pug template rendering
 app.set("view engine", "pug");
-// CHANGE TO PORT 3000 FOR PROJECT SUBMISSION
-const PORT = 5000;
+
+const projects = data.projects;
+
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  let liveLinks = [];
+  for (let i = 0; i < projects.length; i++) {
+    liveLinks += projects[i].live_link;
+    console.log(typeof liveLinks);
+  }
+  res.render("index"), liveLinks;
 });
 
 app.get("/about", (req, res) => {
