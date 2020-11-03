cac64a876ac47ea46372bf02f0224edfd8afa459
Seth Lewis <skiprjak93@protonmail.com>
2020-11-02 19:45:44 -0500

    update app & package, commit .babelrc

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const data = require('./data.json');
 const app = express();
 
 app.set('view engine', 'pug');
+// CHANGE TO PORT 3000 FOR PROJECT SUBMISSION
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/project', (req, res) => {
+    res.render('project');
+});
