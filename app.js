7fc44534074058bee3c0016005b623607ed3197e
sethlewis93 <sbmail616@protonmail.com>
2020-11-04 08:29:29 -0500

    sets up index and about pages

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require("express");
-const data = require("./data.json");
 const bodyParser = require("body-parser");
-
+const { projects } = require("./data.json");
 const app = express();
 
 // Utilize Pug template rendering
 app.set("view engine", "pug");
 
-const projects = data.projects;
-
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
 
 app.get("/", (req, res) => {
-  res.render("index"), projects;
+  res.render("index", { projects });
 });
 
 app.get("/about", (req, res) => {
