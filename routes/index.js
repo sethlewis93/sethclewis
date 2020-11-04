dbd6b3d926752d88fea30a651e10e638e644bb69
Seth Lewis <skiprjak93@protonmail.com>
2020-11-04 10:22:33 -0500

    creates & routes about.js

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,6 @@ const { projects } = require("../data.json");
 router.get("/", (req, res) => {
     res.render("index", { projects });
 });
-
-router.get("/about", (req, res) => {
-    res.render("about");
-});
   
 router.get("/project/:id", (req, res) => {
     const projectId = req.params.id;
