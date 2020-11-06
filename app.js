7f997a5b287f0f3efa0161d8172bca39c16bde50
sethlewis93 <sbmail616@protonmail.com>
2020-11-06 11:16:50 -0500

    renders static files

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
-const indexRoute = require('./routes');
-const aboutRoute = require('./routes/about');
-
+const indexRoute = require("./routes");
+const aboutRoute = require("./routes/about");
 
 app.set("view engine", "pug");
 
 app.use(indexRoute);
-app.use('/about', aboutRoute);
-
+app.use("/about", aboutRoute);
+app.use("/static", express.static("public"));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
