00dbc95018ee1956e9283e5317d97ada9a1388dc
Seth Lewis <skiprjak93@protonmail.com>
2020-11-06 19:07:34 -0500

    updates views, app, index, & css

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const express = require("express");
 const app = express();
 const indexRoute = require("./routes");
 const aboutRoute = require("./routes/about");
+const PORT = 3000;
 
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
 app.set("view engine", "pug");
 
 app.use(indexRoute);
 app.use("/about", aboutRoute);
 app.use("/static", express.static("public"));
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
+/// ERROR HANDLERS ///
+app.use((req, res, next) => {
+    res.status(404).render('not-found');
+});
+
+app.use((err, req, res, next) => {
+    if (err) {
+        console.log('Global error handler called', err);
+      }
+      if (err.status === 404) {
+        res.status(404).render('not-found', { err });
+      } else {
+        err.message = err.message || `Something went wrong on the server.`;
+        res.status(err.status || 500).render('error', { err });
+    }
+});
+
+
--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,23 @@ router.get("/", (req, res) => {
   res.render("index", { projects });
 });
 
-router.get("/project/:id", (req, res) => {
+router.get('/error', (req, res, next) => {
+  const err = new Error();
+  err.message = `Sorry! Something went wrong on the server.`;
+  err.status = 500;
+  throw err;
+});
+
+router.get("/project/:id", (req, res, next) => {
   const projectId = req.params.id;
   const project = projects.find(({ id }) => id === +projectId);
-  // replace w error message later or research sendStatus MDN
-  project ? res.render("project", { project }) : res.sendStatus(404);
+  if(project) {
+    res.render("project", { project }) 
+  } else {
+    const err = new Error();
+    err.status = 404;
+    next(err);
+  }
 });
 
 module.exports = router;
